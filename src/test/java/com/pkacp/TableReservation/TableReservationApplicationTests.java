358c7e7bc230f7ec05504b4b8d5533fd19a46199
pkacp <p.kacprzak.96@gmail.com>
2020-06-06 21:02:30 +0200

    Initial project

---
+++ src/test/java/com/pkacp/TableReservation/TableReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pkacp.TableReservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TableReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
