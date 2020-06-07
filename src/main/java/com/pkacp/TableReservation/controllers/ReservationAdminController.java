17bf04a93cb9f0f277c83b96b1952f8ec84cd1c7
pkacp <p.kacprzak.96@gmail.com>
2020-06-07 18:12:14 +0200

    Added Reservation Controllers

--- src/main/java/com/pkacp/TableReservation/controllers/ReservationAdminController.java
+++ src/main/java/com/pkacp/TableReservation/controllers/ReservationAdminController.java
@@ -7,13 +7,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -21,37 +20,29 @@ import java.util.List;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
-@PreAuthorize("hasRole('CUSTOMER')")
-@RequestMapping("/api/v1/reservations")
-public class ReservationController {
+@PreAuthorize("hasRole('ADMIN')")
+@RequestMapping("/api/v1/admin/reservations")
+public class ReservationAdminController {
 
     @Autowired
     ReservationRepository reservationRepository;
 
     @GetMapping("")
-    public ResponseEntity<List<Reservation>> getCustomerReservations(@RequestParam(required = false) String title) {
+    public ResponseEntity<List<Reservation>> getAllReservations() {
         try {
             List<Reservation> reservations = new ArrayList<>();
+            reservations.addAll(reservationRepository.findAll());
+            if (reservations.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
             return new ResponseEntity<>(reservations, HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
-    @PostMapping
-    public ResponseEntity<Reservation> createReservation(@RequestBody Reservation reservation) {
-        try {
-            Reservation _reservation = reservationRepository
-                    .save(new Reservation(reservation.getSize(), reservation.getStart(), reservation.getEnd()));
-            return new ResponseEntity<>(_reservation, HttpStatus.CREATED);
-        } catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.EXPECTATION_FAILED);
-        }
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<HttpStatus> deleteReservation(@PathVariable("id") long id) {
-        System.out.println(id);
+    @PatchMapping("/{id}")
+    public ResponseEntity<HttpStatus> patchReservation(@PathVariable("id") long id) {
         try {
             reservationRepository.deleteById(id);
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
